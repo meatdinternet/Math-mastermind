62bc20faa698a1fe1ceb1279698dda3ab59e42a0
meatdinternet <jarinakhadgi@yahoo.com>
2026-03-20 17:50:19 +0545

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HF_API_KEY = os.getenv("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", "")
+GROQ_API_KEY = os.getenv("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", "")
